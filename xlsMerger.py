07474f9dfe68ca9be341d1d9db4745851d46b54a
Adam Tolo <adamtolo20@hotmail.com>
2019-03-01 16:01:19 +1000

    added .git folder and log files
    - added .git folder, log file and updated python files
    py: updated variable location and added script finish text

--- xlsMerger.py
+++ xlsMerger.py
@@ -24,6 +24,7 @@ TODO:
 # ------------------------------------------------
 # importing modules
 # ------------------------------------------------
+print("importing modules...")
 
 import arcpy, logging, os, sys
 
@@ -31,73 +32,80 @@ import arcpy, logging, os, sys
 # setting up logger
 # ------------------------------------------------
 def init_logger_singleton():
-	global logger
+    print("setting up logger...")
+    global logger
 
-	logging.basicConfig(level=logging.DEBUG)
-	logger = logging.getLogger(name='mylogger')
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(name='mylogger')
 
-	"""create a file handler"""
-	handler = logging.FileHandler("//pipelinetrust.com.au/apps/GIS/Projects/CCI_Reporting/_py/runtime.log")
-	handler.setLevel(logging.DEBUG)
+    """create a file handler"""
+    handler = logging.FileHandler("//pipelinetrust.com.au/apps/GIS/Projects/CCI_Reporting/_py/runtime.log")
+    handler.setLevel(logging.DEBUG)
 
-	"""create a logging format"""
-	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-	handler.setFormatter(formatter)
+    """create a logging format"""
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
 
-	"""add the handlers to the logger"""
-	logger.addHandler(handler)
+    """add the handlers to the logger"""
+    logger.addHandler(handler)
 
-        logger.info('LOGGER HAS BEEN CREATED AND CAN BE FOUND AT:\n' + handler)
+    logger.info('LOGGER HAS BEEN CREATED AND CAN BE FOUND AT: \n' + str(handler) + '\n')
+
 
 # ------------------------------------------------
 # setting up the environment and variables
 # ------------------------------------------------
-def environment():    
-    arcpy.env.workspace = "//pipelinetrust.com.au/apps/GIS/Projects/CCI_Reporting/
+def environment():
+    logger.info('Setting up environment...\n')
+    arcpy.env.workspace = "//pipelinetrust.com.au/apps/GIS/Projects/CCI_Reporting/"
     arcpy.env.overwriteOutput = True
-    output_gdb = "data.gdb"
-    output_table = "CCI"
-    output_temp_table = "temp"
-    input_files = '_in/'
     
-    	
+
 # ------------------------------------------------
 # deleting existing fc and creating a new one
 # ------------------------------------------------
 def renewFC(input):
-	logger.info('CHECKING IF THE FOLLOWING FEATURE CLASS EXISTS:\n[{0}'.format(input))
-	if arcpy.Exists(input):
-		logger.warning('FILE GEODATABASE FOUND, DELETING & CREATING A NEW ONE...')
-                Delete_management (input)
-		CreateFileGDB_management (workspace, "data.gdb")
-		
-	
+    logger.info('CHECKING IF THE FOLLOWING FEATURE CLASS EXISTS:\n[{0}'.format(input))
+    if arcpy.Exists(input):
+            logger.warning('FILE GEODATABASE FOUND, DELETING & CREATING A NEW ONE...' + '\n')
+            Delete_management (input)
+            CreateFileGDB_management (workspace, "data.gdb")
+
+
 # ------------------------------------------------
 # create a feature table from all spreadsheets in a folder
 # ------------------------------------------------
 def excelToTable(output_temp, output):
+    logger.info('Converting excel data to file geodatabase table...\n' + '\n')
     input_file_list= arcpy.ListFiles('*.xls')
-#    Input_Excel_File=input
     Sheet="Operations"
 
     for xls in input_file_list:
         ExcelToTable_conversion (xls, output_temp, Sheet)
         if not arcpy.Exists(output):
             CreateTable_management (output_gdb, "CCI", output_temp)
-        Append_management (output_temp, output)    
+        Append_management (output_temp, output)
+
+    logger.info('SCRIPT FINISHED')
     
-	
+
+
 # ------------------------------------------------
 # main function
 # ------------------------------------------------
 def main():
-    renewFC(output_gdb)
+    # define global variables
+    output_gdb = "data.gdb"
+    output_table = "CCI"
+    output_temp_table = "temp"
+    input_files = '_in/'
 
+    renewFC(output_gdb)
     excelToTable(output_temp_table, output_table)
 
 
 
 if __name__ == '__main__':
-    environment()
     init_logger_singleton()
+    environment()
     main()
